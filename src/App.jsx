46cd147229c6665721b0f1dac0de7fa3d54af5a0
Nancy K <njkousholt@gmail.com>
2023-05-03 13:41:31 -0400

    new file for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,5 @@
 import InfoBox from './components/InfoBox';
-
-const dataArray = [
-  {
-    title: 'IRA Savings Calculator',
-    description: `Rewiring America has published an IRA Savings Calculator that can
-    get you started on figuring out what savings you may be eligible for.`,
-    link: 'https://www.rewiringamerica.org/app/ira-calculator',
-  },
-  {
-    title: 'Free Energy Audit',
-    description:
-      'New York State Energy Research and Development Authority (NYSERDA) provides you with a top-to-bottom look at where your home is wasting energy.',
-    link: 'https://www.nyserda.ny.gov/All-Programs/Residential-Energy-Assessment-Programs',
-  },
-  {
-    title: 'Free Energy Coaches',
-    description: `New Yorkers for Clean Power has energy coaches that can help you answer your energy questions as well as answer questions you didn't know you had.`,
-    link: 'https://nyforcleanpower.org/cleanenergycoaching/',
-  },
-  {
-    title: 'Learn About Heat Pumps',
-    description: `Using NYSERDA's resources, learn about heating and cooling your home with heat pumps and find a qualified clean heat contractor.`,
-    link: 'https://www.nyserda.ny.gov/All-Programs/Heat-Pump-Program',
-  },
-  {
-    title: 'New Vehicle Tax Credit Eligibility',
-    description:
-      'Learn which newly purchased vehicles may be eligible for a tax credit of up to $7,500.',
-    link: 'https://fueleconomy.gov/feg/tax2023.shtml',
-  },
-  {
-    title: 'Used Vehicle Tax Credit Eligibility',
-    description:
-      'Learn which pre-owned vehicles purchased on or after January 1, 2023 are eligible for a federal income tax credit.',
-    link: 'https://fueleconomy.gov/feg/taxused.shtml',
-  },
-  {
-    title: 'Contact Your Regional Clean Energy Hub',
-    description: `New York plans to establish 12 Regional Clean Energy Hubs that will act as focal points for educating and raising awareness among residents about clean energy. The aim is to encourage participation in the clean energy transition, particularly among people in underserved or disadvantaged communities. Get in touch with your local hub now!`,
-    link: 'https://www.nyserda.ny.gov/All-Programs/Regional-Clean-Energy-Hubs',
-  },
-];
+import guideData from './guide-data';
 
 const App = () => {
   return (
@@ -56,7 +15,7 @@ const App = () => {
           </h2>
         </header>
         <div className="scroll h-full flex flex-col items-center gap-3 pt-3 overflow-auto xl:gap-4">
-          {dataArray.map((data, i) => (
+          {guideData.map((data, i) => (
             <InfoBox key={i + 'guides'} data={data} />
           ))}
         </div>
---
+++ src/guide-data.js
@@ -0,0 +1,43 @@
+const guideData = [
+  {
+    title: 'IRA Savings Calculator',
+    description: `Rewiring America has published an IRA Savings Calculator that can
+    get you started on figuring out what savings you may be eligible for.`,
+    link: 'https://www.rewiringamerica.org/app/ira-calculator',
+  },
+  {
+    title: 'Free Energy Audit',
+    description:
+      'New York State Energy Research and Development Authority (NYSERDA) provides you with a top-to-bottom look at where your home is wasting energy.',
+    link: 'https://www.nyserda.ny.gov/All-Programs/Residential-Energy-Assessment-Programs',
+  },
+  {
+    title: 'Free Energy Coaches',
+    description: `New Yorkers for Clean Power has energy coaches that can help you answer your energy questions as well as answer questions you didn't know you had.`,
+    link: 'https://nyforcleanpower.org/cleanenergycoaching/',
+  },
+  {
+    title: 'Learn About Heat Pumps',
+    description: `Using NYSERDA's resources, learn about heating and cooling your home with heat pumps and find a qualified clean heat contractor.`,
+    link: 'https://www.nyserda.ny.gov/All-Programs/Heat-Pump-Program',
+  },
+  {
+    title: 'New Vehicle Tax Credit Eligibility',
+    description:
+      'Learn which newly purchased vehicles may be eligible for a tax credit of up to $7,500.',
+    link: 'https://fueleconomy.gov/feg/tax2023.shtml',
+  },
+  {
+    title: 'Used Vehicle Tax Credit Eligibility',
+    description:
+      'Learn which pre-owned vehicles purchased on or after January 1, 2023 are eligible for a federal income tax credit.',
+    link: 'https://fueleconomy.gov/feg/taxused.shtml',
+  },
+  {
+    title: 'Contact Your Regional Clean Energy Hub',
+    description: `New York plans to establish 12 Regional Clean Energy Hubs that will act as focal points for educating and raising awareness among residents about clean energy. The aim is to encourage participation in the clean energy transition, particularly among people in underserved or disadvantaged communities. Get in touch with your local hub now!`,
+    link: 'https://www.nyserda.ny.gov/All-Programs/Regional-Clean-Energy-Hubs',
+  },
+];
+
+export default guideData;
