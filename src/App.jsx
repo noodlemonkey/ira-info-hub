dcd07f8fc3b5bc36b89c1baefce1c44fa5904e19
Nancy K <njkousholt@gmail.com>
2023-05-03 13:35:59 -0400

    added scrollbars

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ const dataArray = [
 const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-tertiary-green xl:bg-offwhite">
-      <div className="grid grid-rows-[auto_full] h-[calc(100dvh-10px)] w-[calc(100dvw-10px)] bg-offwhite border rounded-lg border-tertiary-green p-3 divide-y-2 overflow-hidden xl:border-4 xl:w-[80%] xl:h-[90%] xl:p-5">
+      <div className="grid grid-rows-[auto_full] h-[calc(100dvh-10px)] w-[calc(100dvw-10px)] bg-offwhite border rounded-lg border-tertiary-green p-3 overflow-hidden xl:border-4 xl:w-[80%] xl:h-[90%] xl:p-5">
         <header className="flex flex-col justify-center items-center pb-3">
           <h1 className="font-archivo-eb text-primary-green text-2xl">
             IRA Info Hub
@@ -55,9 +55,9 @@ const App = () => {
             which the Inflation Reduction Act can benefit you.
           </h2>
         </header>
-        <div className="no-scrollbar h-full flex flex-col items-center gap-3 pt-3 overflow-auto xl:gap-4">
-          {dataArray.map((data) => (
-            <InfoBox data={data} />
+        <div className="scroll h-full flex flex-col items-center gap-3 pt-3 overflow-auto xl:gap-4">
+          {dataArray.map((data, i) => (
+            <InfoBox key={i + 'guides'} data={data} />
           ))}
         </div>
       </div>
--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -2,7 +2,7 @@ import Button from './Button';
 
 const InfoBox = ({ data }) => {
   return (
-    <div className="flex flex-col justify-center items-center text-center border-2 border-quaternary-green rounded-md p-3 gap-1 xl:gap-3 xl:w-[90%] xl:p-5">
+    <div className="flex flex-col w-[95%] justify-center items-center text-center border-2 border-quaternary-green rounded-md p-3 gap-1 xl:gap-3 md:w-[80%] xl:p-5">
       <Button text={data.title} link={data.link} />
       <p className="font-archivo text-primary-green xl:text-lg">
         {data.description}
