47be247771e69c2d1a87d98a5afd2b636818845c
Nancy K <njkousholt@gmail.com>
2023-05-03 17:27:32 -0400

    changed wording

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ const App = () => {
             IRA Info Hub
           </h1>
           <h2 className="font-archivo-li text-md text-secondary-green text-center md:text-lg">
-            Here are some helpful links to guide New Yorkers through the ways in
-            which the Inflation Reduction Act can benefit you.
+            Here are some helpful links to guide you through the ways in which
+            the Inflation Reduction Act can benefit New Yorkers.
           </h2>
           <hr className="w-[80%] mt-2 border-t border-solid border-quaternary-green" />
         </header>
